b79e11b4663a45f3661fc827c5960e0460f2d05f
johnzieman <j.g.zokirov@gmail.com>
2021-09-30 12:49:40 +0500

    add logo, starter initializations with material navigation elements v000.1

--- app/src/main/java/com/johnzieman/ziemapp/drinkwater/MainActivity.kt
+++ app/src/main/java/com/johnzieman/ziemapp/drinkwater/MainActivity.kt
@@ -2,10 +2,19 @@ package com.johnzieman.ziemapp.drinkwater
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var navController: NavController
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = Navigation.findNavController(this, R.id.nav_host)
+        bottomNavigationView = findViewById(R.id.bottom_nav)
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
